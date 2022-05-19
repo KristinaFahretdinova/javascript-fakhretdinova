f8c49d908b016713d159a54d7e83fc7ee5b87b47
KristinaFahretdinova <80529123+KristinaFahretdinova@users.noreply.github.com>
2022-05-19 20:22:17 +0500

    ЛР №1-2

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<p> Дата и время:
+<p>
+<?php
+$d=date("d.m.Y H:i");
+echo($d);
+?>
+	<ul>
+	<br></br>
+	<p><b>Лабораторная работа №1</b></p>
+	<p>Введение в JavaScript</p>
+        <li>
+            <a href='/Lab1.html'>Задание 1</a>
+        </li>
+        <li>
+            <a href='/Lab2.html'>Задание 2</a>
+        </li>
+        <li>
+            <a href='/Lab3.html'>Задание 3</a>
+        </li>
+		<li>
+            <a href='/Lab4.html'>Задание 4</a>
+        </li>
+        <li>
+            <a href='/Lab5.html'>Задание 5</a>
+        </li>
+        <li>
+            <a href='/Lab6.html'>Задание 6</a>
+        </li>
+		<li>
+            <a href='/Lab7.html'>Задание 7</a>
+        </li>
+		
+	<p><b>Лабораторная работа №2</b></p>
+	<p>События и их обработка в JavaScript и jQuery</p>
+	<li>
+            <a href='/lab2/lab_2-1.html'>Задание 1</a>
+        </li>
+        <li>
+            <a href='/lab2/lab_2-2.html'>Задание 2</a>
+        </li>
+        <li>
+            <a href='/lab2/lab_2-3.html'>Задание 3</a>
+        </li>
+		<li>
+            <a href='/lab2/lab_2-4.html'>Задание 4</a>
+        </li>
+        <li>
+            <a href='/lab2/lab_2-5.html'>Задание 5</a>
+        </li>
+        <li>
+            <a href='/lab2/lab_2-6.html'>Задание 6</a>
+        </li>
+		<li>
+            <a href='/lab2/lab_2-7.html'>Задание 7</a>
+        </li>
+	
+	
+	</ul>
