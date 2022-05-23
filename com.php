bbbe90942155fec13f42ed196951ea8c8ba6e54f
KristinaFahretdinova <80529123+KristinaFahretdinova@users.noreply.github.com>
2022-05-23 17:30:36 +0500

    Лабораторная работа №3

---
+++ com.php
@@ -0,0 +1,16 @@
+﻿<?php
+	$xml = simplexml_load_file("comment.xml");
+	$tek = count($xml->comments);
+	$s=0;
+	for($i = 0; $i <= $tek; $i++){
+		$s++;
+		echo("<br><div class='news'>");
+        echo "ФИО: ".$xml->comments[$i]->name . "<br>";
+		echo "Факультет: ".$xml->comments[$i]->faculty . "<br>";
+		echo "Почта: ".$xml->comments[$i]->email . "<br>";
+		echo "Отзыв: ".$xml->comments[$i]->comment . "<br></div>";
+		if($s==$tek){
+            break;
+		}
+	}
+?>
